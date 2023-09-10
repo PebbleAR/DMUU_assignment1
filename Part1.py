409a218159b1f2977cc241fcdd4c00b467832c76
Annuhh <144354920+Annuhh@users.noreply.github.com>
2023-09-10 17:51:13 +0200

    Update Part1.py

--- Part1.py
+++ Part1.py
@@ -3,6 +3,7 @@ from scipy.stats import binom, uniform, poisson
 import matplotlib.pyplot as plt
 import math
 
+# Generating mu
 def generate_mu():
     Y = 0
     while not 1 <= Y <= 10:
@@ -19,6 +20,25 @@ def generate_mus(n):
 
 mus = generate_mus(15)
 mus
+
+# Generating sigma
+t_x = 2.0736
+def Beta_pdf(x):
+    60 * x**2 *(1-x)**3
+    
+def generate_sigma():
+    while True
+        Y = uniform.rvs(size = 1)
+        U = uniform.rvs(size = 1)
+        if U <= Beta_pdf(Y)/t_x:
+            X = Y
+            break
+    return X
+
+sigma = []
+for i in range(0,15):
+    sigma.append(generate_sigma())
+    
 
 #[150.15256811493003, 115.19677491162399, 111.67979915328472, 134.30643467186982, 126.56929525374949, 195.00577403381462, 172.5978737876039, 118.55164097302003, 136.05687557651135, 134.747749160613, 122.0422866146018, 162.73107320992452, 197.7589332244999, 169.49703379075925, 115.6649611292763]
 
