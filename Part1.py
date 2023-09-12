8441b6dc4ae5cdc1fea537015551e81fd3bcc958
nienkevdhurk <90247055+nienkevdhurk@users.noreply.github.com>
2023-09-12 16:58:53 +0200

    Update Part1.py
    code samengevoegd

--- Part1.py
+++ Part1.py
@@ -14,19 +14,11 @@ def generate_mu():
     mu = 100 + 10 * Y
     return mu[0]
 
-def generate_mus(n):
-    mus = []
-    for _ in range(n):
-        mus.append(generate_mu())
-    return mus
-
-mus = generate_mus(15)
-mus
-
 # Generating sigma
 t_x = 2.0736
+
 def Beta_pdf(x):
-    60 * x**2 *(1-x)**3
+    return 60 * x**2 *(1-x)**3
     
 def generate_sigma():
     while True:
@@ -35,42 +27,47 @@ def generate_sigma():
         if U <= Beta_pdf(Y)/t_x:
             X = Y
             break
-    return X
-
-sigma = []
-for i in range(0,15):
-    sigma.append(generate_sigma())
+    return X[0]
 
 # Generating Unit revenue
+p1 = 1/3
+p2 = 1 - p1
+
 def generate_uniform():
     U_1 = uniform.rvs(size = 1)
     U_2 = uniform.rvs(size = 1)
     return U_1, U_2
 
-def generate_X(p1, p2):
+def generate_X(p1):
     U = generate_uniform()
     if p1 < U[0][0]:
         X = -(1/2)*math.log(U[1][0])
     else:
-        print(U[1][0])
         X = U[1][0]
     return X
 
-n=15
-p1 = 1/3
-p2 = 2/3
-R = []
-for i in range(0,n):
-    x = generate_X(p1, p2)
+def generate_R():
+    x = generate_X(p1)
     if x < 5/2:
         r = 15+2*x
     else:
         r = 15+2*(5/2)
-    R.append(r)
-print(R)
-    
+    return r
 
-#[150.15256811493003, 115.19677491162399, 111.67979915328472, 134.30643467186982, 126.56929525374949, 195.00577403381462, 172.5978737876039, 118.55164097302003, 136.05687557651135, 134.747749160613, 122.0422866146018, 162.73107320992452, 197.7589332244999, 169.49703379075925, 115.6649611292763]
+# Generate data
+n = 15
+def generate_data(n):
+    mus, sigmas, R = [], [], []
+    for _ in range(n):
+        mus.append(generate_mu())
+        sigmas.append(generate_sigma())
+        R.append(generate_R())
+    return mus, sigmas, R
 
+data = generate_data(n)
 
-# %%
+print(data[0])
+print()
+print(data[1])
+print()
+print(data[2])
